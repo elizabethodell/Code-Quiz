24e316d48b19cd0fcc463a77274a1cfb26271c38
ebo16@vt.edu <ebo16@vt.edu>
2021-09-20 15:52:30 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+// Coding Quiz Challenge
+//Try to answer the following code related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!
+//Q: Commonly used data types DO Not Included:
+//1. strings 2. booleans 3. alerts 4. numbers
+//Q: The condition in an if / else statement is enclosed with
+//1. quotes 2. curly brackets 3. parameters 4. square brackets
+//Q: Arrays in JavaScript can be used to store
+//1. numbers and strings 2. other arrays 3. booleans 4. all of the above
+//Q: String values must be enclosed within __ when being assigned to variables.
+//1. commas 2. curly brackets 3. quotes 4. parenthesis
+//Q: A very useful tool used during development and debugging for printing content to the debugger is:
+//1. JavaScript 2. terminal/bash 3. for loops 4. console log
+//All done!
+//Your final score is __.
+//Enter your initials SUBMIT
